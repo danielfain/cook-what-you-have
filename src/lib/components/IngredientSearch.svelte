b9fa7d03de038a8aa45584444803f77c5fb7e4ed
Daniel Fain <daniel@fain.dev>
2024-10-26 03:20:09 -0400

    use shadcn for search bar

--- src/lib/components/IngredientSearch.svelte
+++ src/lib/components/IngredientSearch.svelte
@@ -1,40 +1,77 @@
 <script lang="ts">
 	import {
+		Command,
+		CommandEmpty,
+		CommandGroup,
+		CommandInput,
+		CommandItem
+	} from '$lib/components/ui/command';
+	import {
 		searchIngredients,
 		selectedIngredients,
 		type Ingredient
 	} from '$lib/stores/ingredients.svelte.ts';
+	import { slide } from 'svelte/transition';
 
-	let ingredientQuery = $state('');
-	let ingredientResults = $state<Ingredient[]>([]);
+	let isOpen = $state(false);
+	let query = $state('');
+	let searchResults = $state<Ingredient[]>([]);
 
 	function handleInput() {
-		ingredientResults = searchIngredients(ingredientQuery);
+		// TODO: Debounce
+		searchResults = searchIngredients(query);
 	}
 
-	function handleAddIngredient(ingredient: Ingredient) {
+	function handleSelect(ingredient: Ingredient) {
 		selectedIngredients.add(ingredient);
-		ingredientQuery = '';
-		ingredientResults = [];
+		query = '';
+		searchResults = [];
+		isOpen = false;
 	}
+
+	$inspect(searchResults);
 </script>
 
-<div class="w-full">
-	<input
-		class="w-full p-1 text-s"
-		type="text"
-		bind:value={ingredientQuery}
-		oninput={handleInput}
-		placeholder="Search ingredients..."
-	/>
+<Command class="rounded-lg border shadow-md">
+	<CommandInput placeholder="Search ingredients..." bind:value={query} oninput={handleInput} />
 
-	{#if ingredientQuery && ingredientResults.length > 0}
-		<ul class="w-full max-h-52 p-0 mt-1">
-			{#each ingredientResults as ingredient}
-				<li class="p-1 hover:bg-slate-300" onclick={() => handleAddIngredient(ingredient)}>
-					{ingredient.name}
-				</li>
-			{/each}
-		</ul>
+	{#if query || isOpen}
+		<div class="max-h-[300px] overflow-y-auto" transition:slide|local={{ duration: 200 }}>
+			{#if searchResults.length === 0}
+				<CommandEmpty>No ingredients found.</CommandEmpty>
+			{:else}
+				<CommandGroup>
+					{#each searchResults as ingredient}
+						<CommandItem
+							value={ingredient.name}
+							onclick={() => handleSelect(ingredient)}
+							class="cursor-pointer"
+						>
+							<span>{ingredient.name}</span>
+						</CommandItem>
+					{/each}
+				</CommandGroup>
+			{/if}
+		</div>
 	{/if}
-</div>
+</Command>
+
+<style lang="postcss">
+	.overflow-y-auto {
+		scrollbar-width: thin;
+		scrollbar-color: theme(colors.gray.200) transparent;
+	}
+
+	.overflow-y-auto::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	.overflow-y-auto::-webkit-scrollbar-track {
+		background: transparent;
+	}
+
+	.overflow-y-auto::-webkit-scrollbar-thumb {
+		background-color: theme(colors.gray.200);
+		border-radius: 3px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,8 @@
 	import SelectedIngredients from '$lib/components/SelectedIngredients.svelte';
 </script>
 
-<div class="flex flex-col items-center w-full">
-	<h1 class="p-3 items-center">Ingredients</h1>
-
-	<div class="w-3/4">
+<div class="flex flex-col items-center m-5">
+	<div class="w-3/4 mb-5">
 		<IngredientSearch />
 	</div>
 
